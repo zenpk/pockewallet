585f47d1c13fa809feb423b25ac5b738bf32d72a
zenpk <99558412+zenpk@users.noreply.github.com>
2024-07-01 00:21:15 +0800

    add pwa configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,31 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Pockewallet",
+        short_name: "Pockewallet",
+        description: "Keep track of your expenses",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "any",
+        start_url: "./",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
